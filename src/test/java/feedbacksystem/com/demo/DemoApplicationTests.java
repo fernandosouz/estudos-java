8a34588cf2a4d631c9a5c8b1a19db79c383b5490
Fernando Souza <souzafernandodev@gmail.com>
2019-05-29 07:35:33 -0300

    test

--- src/test/java/feedbacksystem/com/demo/DemoApplicationTests.java
+++ src/test/java/feedbacksystem/com/demo/DemoApplicationTests.java
@@ -1,16 +1,15 @@
 package feedbacksystem.com.demo;
 
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		Assert.assertTrue(true);
 	}
 
 }
