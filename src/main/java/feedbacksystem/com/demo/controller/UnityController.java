bc751c958885f5eb8b06267c08aa8d2c355294c5
Fernando Souza <souzafernandodev@gmail.com>
2019-10-11 21:54:37 -0300

    ajuste nps

--- src/main/java/feedbacksystem/com/demo/controller/UnityController.java
+++ src/main/java/feedbacksystem/com/demo/controller/UnityController.java
@@ -1,11 +1,18 @@
 package feedbacksystem.com.demo.controller;
 
+import feedbacksystem.com.demo.GetDatasToChartRequest;
 import feedbacksystem.com.demo.model.Unity;
+import feedbacksystem.com.demo.model.responses.chart.DataToChartOptimized;
 import feedbacksystem.com.demo.repository.UnityRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @CrossOrigin(origins = "*")
@@ -27,6 +34,20 @@ public class UnityController {
     @GetMapping(value = "/unity")
     public ResponseEntity getAll() {
         return new ResponseEntity(unityRepository.findAll(), HttpStatus.CREATED);
+    }
+
+    @PostMapping(value = "/nps")
+    public ResponseEntity getByid(@RequestBody GetDatasToChartRequest getDatasToChartRequest) throws ParseException {
+
+        Unity unity = unityRepository.findById(getDatasToChartRequest.getUnityId()).get();
+
+        Date start = new SimpleDateFormat("yyyy-MM-dd").parse(getDatasToChartRequest.getStartDate());
+        /*TODO ver outra forma de adicionar um à data final*/
+        Date end = new Date(new SimpleDateFormat("yyyy-MM-dd").parse(getDatasToChartRequest.getEndDate()).getTime() + (1000 * 60 * 60 * 24));
+
+        List<DataToChartOptimized> dataToChartOptimizeds = unityRepository.getSumBetweenDatesByQuestion(unity.getIdNpsQuestion(), start, end );
+
+        return new ResponseEntity(dataToChartOptimizeds, HttpStatus.OK);
     }
 
 
--- src/main/java/feedbacksystem/com/demo/repository/UnityRepository.java
+++ src/main/java/feedbacksystem/com/demo/repository/UnityRepository.java
@@ -2,12 +2,14 @@ package feedbacksystem.com.demo.repository;
 
 import feedbacksystem.com.demo.model.Question;
 import feedbacksystem.com.demo.model.Unity;
+import feedbacksystem.com.demo.model.responses.chart.DataToChartOptimized;
 import feedbacksystem.com.demo.model.responses.textresponse.IdNameOfUnity;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -18,6 +20,34 @@ public interface UnityRepository extends CrudRepository<Unity, Long> {
 
     @Query(value="SELECT u.* FROM unity u WHERE company_id = :companyId", nativeQuery = true)
     List<Unity> findunitsByCompanyId(@Param("companyId") Long companyId);
+
+    @Query(value=" SELECT\n" +
+            "    u.id                    AS unityId,\n" +
+            "    q.description           AS questionDescription,\n" +
+            "    q.id                    AS questionId,\n" +
+            "    pr.description          AS predefinedResponseDescription,\n" +
+            "    pr.id                   AS predefinedId,\n" +
+            "    pr.is_positive_response AS isPositiveResponse,\n" +
+            "    CASE\n" +
+            "        WHEN (sum(rc.count) ISNULL)\n" +
+            "            THEN 0\n" +
+            "            ELSE sum(rc.count)\n" +
+            "    END                     AS sum\n" +
+            " from predefined_response pr\n" +
+            "    LEFT JOIN response_count rc on (pr.id = rc.predefined_response_id AND rc.created_date_time BETWEEN :start AND :end)\n" +
+            "    INNER JOIN question q on pr.question_id = q.id\n" +
+            "    INNER JOIN unity u on q.unity_id = u.id\n" +
+            " WHERE\n" +
+            "    q.id = :questionId \n" +
+            "    AND q.question_type = 1\n" +
+            "    AND q.show_on_dash_board = true\n" +
+            " GROUP BY\n" +
+            "    u.id,\n" +
+            "    q.description,\n" +
+            "    q.id,\n" +
+            "    pr.description,\n" +
+            "    pr.id ", nativeQuery = true)
+    List<DataToChartOptimized> getSumBetweenDatesByQuestion(@Param("questionId") Long questionId, @Param("start") Date start, @Param("end") Date end);
 
 
 
